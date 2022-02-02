34b904ce78c5f2a82ae3c38b2ff453b81fbe60e0
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-01 18:00:30 -0700

    Create App class

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import '../css/App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ideas: [
+        {id: 1, title:'Hug Dingo', description:'My boy could use a big hug right now'},
+        {id: 2, title:'Walk Teddy', description:'Must walk my demon so he does not hurt me'},
+        {id: 3, title:'Kiss Travie', description:'Wanna kisss zaddy'},
+      ]
+    }
+  }
+
+render() {
+  return (
+    <main className='App'>
+      <h1>IdeaBox</h1>
+    </main>
+  )
+  }
+}
+
+export default App;
