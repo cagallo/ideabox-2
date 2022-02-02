080af503448a8ee798b89f50ffd57d7f9e4644f4
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-01 20:57:13 -0700

    Finish delete functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,12 @@ import React from 'react';
 import '../css/Card.css';
 
 
-const Card = ({title, description, id}) => {
+const Card = ({title, description, id, deleteIdea}) => {
     return (
         <div className='card'>
             <h3>{title}</h3> 
             <p>{description}</p>
-            <button>🗑</button>
+            <button onClick={() => deleteIdea(id)}>🗑</button>
         </div>
     )
 }
